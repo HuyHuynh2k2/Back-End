19b93085fb80821b97fb4bf431cc7f8a0654ca2c
huyhuynh <huynhlonghuy.work@gmail.com>
2024-10-25 22:12:46 -0700

    create book routes

---
+++ src/routes/open/books.ts
@@ -0,0 +1,8 @@
+import express, { NextFunction, Request, Response, Router } from 'express';
+import { pool, validationFunctions } from '../../core/utilities';
+
+const bookRouter: Router = express.Router();
+
+const isStringProvided = validationFunctions.isStringProvided;
+
+export { bookRouter };
