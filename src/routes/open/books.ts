e4011df56342e1a313d16a9ae3b547a1cd48b4a7
huyhuynh <huynhlonghuy.work@gmail.com>
2024-10-26 22:30:13 -0700

    retrieve authors/original title/ all

--- src/routes/open/books.ts
+++ src/routes/open/books.ts
@@ -32,7 +32,7 @@ function mwValidPaginationQuery(
 
 // Middleware to validate the ISBN parameter
 function mwValidISBN(request: Request, response: Response, next: NextFunction) {
-    const isbn: string = request.params.isbn;
+    const isbn: string = request.query.isbn as string; // Cast to string for validation
     if (validationFunctions.isNumberProvided(isbn) && isbn.length === 13) {
         next();
     } else {
@@ -43,27 +43,107 @@ function mwValidISBN(request: Request, response: Response, next: NextFunction) {
     }
 }
 
+bookRouter.get('/', mwValidISBN, (request: Request, response: Response) => {
+    const theQuery = 'SELECT * FROM BOOKS WHERE isbn13 = $1';
+    const values = [request.query.isbn];
+
+    pool.query(theQuery, values)
+        .then((result) => {
+            if (result.rowCount === 1) {
+                response.send({
+                    book: result.rows[0],
+                });
+            } else {
+                response.status(404).send({
+                    message: 'Book not found',
+                });
+            }
+        })
+        .catch((error) => {
+            console.error('DB Query error on GET book by ISBN');
+            console.error(error);
+            response.status(500).send({
+                message: 'Server error - contact support HUY HUYNH',
+            });
+        });
+});
+
 bookRouter.get(
-    '/:isbn',
-    mwValidISBN,
+    '/author/:author',
     (request: Request, response: Response, next: NextFunction) => {
-        const theQuery = 'SELECT * FROM BOOKS WHERE isbn13 = $1';
-        const values = [request.params.isbn];
+        const theQuery = 'SELECT * FROM BOOKS WHERE authors = $1';
+        const values = [request.params.author]; // Accessing the author from request.params
 
         pool.query(theQuery, values)
             .then((result) => {
-                if (result.rowCount === 1) {
+                if (result.rowCount > 0) {
                     response.send({
-                        book: result.rows[0],
+                        books: result.rows,
                     });
                 } else {
                     response.status(404).send({
-                        message: 'Book not found',
+                        message: 'No books found for this author',
                     });
                 }
             })
             .catch((error) => {
-                console.error('DB Query error on GET book by ISBN');
+                console.error('DB query error on GET books by author');
+                console.error(error);
+                response.status(500).send({
+                    message: 'Server error - contact support HUY HUYNH',
+                });
+            });
+    }
+);
+
+bookRouter.get(
+    '/original_title/:original_title',
+    (request: Request, response: Response) => {
+        const theQuery = 'SELECT * FROM BOOKS WHERE original_title = $1';
+        const values = [request.params.original_title];
+
+        pool.query(theQuery, values)
+            .then((result) => {
+                if (result.rowCount > 0) {
+                    response.send({
+                        books: result.rows,
+                    });
+                } else {
+                    response.status(404).send({
+                        message: 'No books found with given original title',
+                    });
+                }
+            })
+            .catch((error) => {
+                console.error('DB query error on GET books by original title');
+                console.error(error);
+                response.status(500).send({
+                    message: 'Server error - contact support HUY HUYNH',
+                });
+            });
+    }
+);
+bookRouter.get(
+    '/all',
+    mwValidPaginationQuery,
+    (request: Request, response: Response) => {
+        const page: number = parseInt(request.query.page as string, 10);
+        const limit: number = parseInt(request.query.limit as string, 10);
+        const offset: number = (page - 1) * limit;
+
+        const theQuery = 'SELECT * FROM BOOKS LIMIT $1 OFFSET $2';
+        const values = [limit, offset];
+
+        pool.query(theQuery, values)
+            .then((result) => {
+                response.send({
+                    books: result.rows,
+                });
+            })
+            .catch((error) => {
+                console.error(
+                    'DB query error on GET all books with pagination'
+                );
                 console.error(error);
                 response.status(500).send({
                     message: 'Server error - contact support HUY HUYNH',
